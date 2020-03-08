cf041744ecd4de02c75b0a33e476d4544f5b47dc
Colleen Noel <noel.colleen@gmail.com>
2020-03-08 15:29:03 -0400

    tests

---
+++ tests/strictEqual.test.js
@@ -0,0 +1,24 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const expect = require('chai').expect
+
+describe('strictEqualFile', () => {
+  describe('strictEqual()', () => {
+    it('returns true when the values are the same', () => {
+      // test data needed
+      //running the code to be tested
+      //assertions - expect
+    })
+    it('returns false when values are not the same', () => {
+      // test data needed
+      //running the code to be tested
+      //assertions - expect
+    })
+    it('returns false when types are not the same', () => {
+      // test data needed
+      //running the code to be tested
+      //assertions - expect
+
+    })
+  })
+})
